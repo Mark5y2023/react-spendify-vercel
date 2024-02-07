ec3c3a6e3f1df84e9ee3da810841acf18e3c7b10
Your Name <denmarkjudilla22@gmail.com>
2024-02-07 14:24:11 +0800

    Update 2.7.24

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, TextField, Snackbar, Alert } from '@mui/material';
 import './Home.css'; // Import the CSS file for styling
